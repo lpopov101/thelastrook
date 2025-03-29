bdc4a36cd2c06beab8a34d4e69e94c0b5db23702
lpopov101 <luben.popov@gmail.com>
2025-03-29 14:35:18 -0400

    king collision detection poc

--- src/rook.gd
+++ src/rook.gd
@@ -4,13 +4,16 @@ class_name Rook extends CharacterBody2D
 
 var cur_move_dir = Vector2.ZERO
 
-
-
 func _physics_process(delta: float):
 	var input_move_dir = Global.input_manager.get_move_dir()
 	update_move_dir(input_move_dir)
 	velocity = cur_move_dir * speed * delta
 	move_and_slide()
+	var collision = get_last_slide_collision()
+	if collision != null:
+		var collider = collision.get_collider()
+		if collider is KingCharacter:
+			print("hit king")
 
 func update_move_dir(input_move_dir: Vector2):
 	if not is_zero_approx(input_move_dir.y):
