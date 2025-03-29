003387b7b03db394ec881a72cc079cff33f44b6c
lpopov101 <luben.popov@gmail.com>
2025-03-29 14:42:14 -0400

    fix

--- src/rook.gd
+++ src/rook.gd
@@ -9,6 +9,8 @@ func _physics_process(delta: float):
 	update_move_dir(input_move_dir)
 	velocity = cur_move_dir * speed * delta
 	move_and_slide()
+	if get_slide_collision_count() == 0:
+		return
 	for collision_idx in [0, get_slide_collision_count()]:
 		var collision = get_slide_collision(collision_idx)
 		if collision != null:
