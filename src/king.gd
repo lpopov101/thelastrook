99ad6d9c501852138daf71d44719f030b71792bc
oe555 <oweneckart@outlook.com>
2025-03-29 11:37:34 -0400

    King

--- src/king.gd
+++ src/king.gd
@@ -1,11 +1,31 @@
 class_name King extends Node2D
 
-# An integer from 0-7 describing which direction the king is moving
-var direction: int
 # The max (Manhattan) distance in pixels from the center the king is willing to move
-var MAX_DIST: int
+var MAX_DIST = 300
+# Self explanatory
+var SPEED = 50
+signal DamagedKing
 
+# Other not important stuff
+var direction = 0
 var pi = 3*acos(.5)
+var dx = 0
+var dy = 0
+
+@onready var king: King = $"."
+
+func _ready() -> void:
+	_on_king_movement_timer_timeout()
 
 func _on_king_movement_timer_timeout() -> void:
-	print(pi)
+	direction = randi() % 8
+	dx = cos(direction * pi/4)
+	dy = sin(direction * pi/4)
+
+func _physics_process(delta: float) -> void:
+	if (abs(self.position.x + (delta * SPEED * dx)) + abs(self.position.y + (delta * SPEED * dy)) < MAX_DIST):
+		self.position.x += delta * SPEED * dx
+		self.position.y += delta * SPEED * dy
+
+func _on_king_hitbox_body_entered(body: Node2D) -> void:
+	DamagedKing.emit()
