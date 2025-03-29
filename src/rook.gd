1a2136c17bc3aa05ad4b82a2e5d299c3fc9f5bc7
lpopov101 <luben.popov@gmail.com>
2025-03-29 14:17:40 -0400

    Refactor rook and input manager in GDScript

---
+++ src/rook.gd
@@ -0,0 +1,19 @@
+class_name Rook extends CharacterBody2D
+
+@export var speed = 10000
+
+var cur_move_dir = Vector2.ZERO
+
+
+
+func _physics_process(delta: float):
+	var input_move_dir = Global.input_manager.get_move_dir()
+	update_move_dir(input_move_dir)
+	velocity = cur_move_dir * speed * delta
+	move_and_slide()
+
+func update_move_dir(input_move_dir: Vector2):
+	if not is_zero_approx(input_move_dir.y):
+		cur_move_dir = Vector2(0, input_move_dir.y)
+	elif not is_zero_approx(input_move_dir.x):
+		cur_move_dir = Vector2(input_move_dir.x, 0)
