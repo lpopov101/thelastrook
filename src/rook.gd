fe7b710e53cca836533efce8fd87f451024b89eb
oe555 <oweneckart@outlook.com>
2025-03-29 20:07:30 -0400

    Basic castling

--- src/rook.gd
+++ src/rook.gd
@@ -4,12 +4,21 @@ class_name Rook extends CharacterBody2D
 
 var cur_move_dir = Vector2.ZERO
 
+@onready var king: KingCharacter = $"../King"
+
 func _physics_process(_delta: float):
 	var input_move_dir = Global.input_manager.get_just_pressed_move_dir()
 	update_move_dir(input_move_dir)
 	process_collisions()
 	velocity = cur_move_dir * speed
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("Castle"):
+		var temp_king = king.global_position
+		var temp_rook = global_position
+		global_position = Vector2(-1000, -1000)
+		king.global_position = temp_rook
+		global_position = temp_king
 
 func update_move_dir(input_move_dir: Vector2):
 	if not is_zero_approx(input_move_dir.y):
