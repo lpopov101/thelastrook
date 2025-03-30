712069493ad8276197ecb372fdb75800877fede4
Zaid Alsaigh <zalsaigh@uwaterloo.ca>
2025-03-30 00:21:11 -0400

    Integrate pawn spawning into john scene

--- src/test_scene_john.gd
+++ src/test_scene_john.gd
@@ -2,6 +2,33 @@ class_name JohnTest
 extends Node2D
 
 const TEST_SCENE = preload("res://scenes/test_scene_john.tscn")
+const PAWN_SCENE : PackedScene = preload("uid://mmkclxn06vhw") # pawn.tscn
+
+func _ready():
+	$PawnSpawnTimer.timeout.connect(_on_pawn_spawn_timer_timeout)
+	$PawnSpawnTimer.start()
+
+func _on_pawn_spawn_timer_timeout():	
+	var pawn_spawn_location : PathFollow2D = get_node_or_null("PawnSpawnPath/PawnSpawnLocation")
+	
+	if not pawn_spawn_location:
+		print("Error! Pawn spawn location couldn't be gotten!")
+		return
+	
+	# we can change this later to see if we want to spawn pawns away from the king
+	pawn_spawn_location.progress_ratio = randf()
+	_spawn_pawn_at_position(pawn_spawn_location)
+
+func _spawn_pawn_at_position(pawn_spawn_location: PathFollow2D):
+	var pawn : Pawn = PAWN_SCENE.instantiate()
+	pawn.position = pawn_spawn_location.position
+
+	# set pawn's direction perpendicular to the path direction.
+	var rotation_inwards = pawn_spawn_location.rotation + PI / 2
+	pawn.set_velocity_with_rotation(rotation_inwards)
+
+	# spawn the pawn
+	add_child(pawn)
 
 static func new_scene() -> JohnTest:
 	var new_s = TEST_SCENE.instantiate()
