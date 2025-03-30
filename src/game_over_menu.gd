063b822485f48ef2b9b4b64bcea1cf92cca6b09d
John Simmons <jsimmons2705@gmail.com>
2025-03-30 16:03:40 -0400

    Game score on game over and call_deferred

--- src/game_over_menu.gd
+++ src/game_over_menu.gd
@@ -3,8 +3,14 @@ extends Control
 
 const GAME_OVER_MENU = preload("res://scenes/game_over_menu.tscn")
 
+@onready var wave_label: Label = $PanelContainer/VBoxContainer/WaveLabel
+
 static func new_scene() -> GameOverMenu:
 	return GAME_OVER_MENU.instantiate()
+	
+
+func _ready() -> void:
+	wave_label.text = "Wave: " + str(Global.game_manager.wave)
 
 
 func _on_new_game_btn_pressed() -> void:
