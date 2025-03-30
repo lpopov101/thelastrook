113e5e763da01e4324e39206cf7f5010d8d0ffcd
lpopov101 <luben.popov@gmail.com>
2025-03-30 13:53:59 -0400

    Fix and hook up to pawn

--- src/audio_manager.gd
+++ src/audio_manager.gd
@@ -1,19 +1,21 @@
 class_name AudioManager extends Node
 
 func _ready() -> void:
-    Global.audio_manager = self
+	Global.audio_manager = self
 
 func play_sound(sound: AudioStream, loop: bool = false, volume: int = -10) -> AudioStreamPlayer:
-    var audio_stream_player = AudioStreamPlayer.new()
-    audio_stream_player.stream = sound
-    audio_stream_player.volume_db = volume
-    if loop:
-        audio_stream_player.loop = true
-    else:
-        audio_stream_player.finished.connect("on_audio_finished", audio_stream_player)
-    add_child(audio_stream_player)
-    audio_stream_player.play()
-    return audio_stream_player
+	var audio_stream_player = AudioStreamPlayer.new()
+	audio_stream_player.stream = sound
+	audio_stream_player.volume_db = volume
+	if loop:
+		audio_stream_player.loop = true
+	else:
+		audio_stream_player.finished.connect(func():
+			on_audio_finished(audio_stream_player)
+		)
+	add_child(audio_stream_player)
+	audio_stream_player.play()
+	return audio_stream_player
 
 func on_audio_finished(audio_stream_player: AudioStreamPlayer) -> void:
-    audio_stream_player.queue_free()
+	audio_stream_player.queue_free()
