eb2611df00c3d09587d6c2742427885ee3e1c771
John Simmons <jsimmons2705@gmail.com>
2025-03-30 09:53:33 -0400

    Added groups

--- src/global.gd
+++ src/global.gd
@@ -3,6 +3,10 @@ extends Node
 var game_manager: GameManager
 var input_manager: InputManager
 
+const ENEMY_GROUP = "enemy"
+const ENEMY_ATTACK_GROUP = "enemyAttack"
+const PLAYER_ATTACK_GROUP = "playerAttack"
+
 func _ready() -> void:
 	input_manager = InputManager.new()
 	add_child(input_manager)
